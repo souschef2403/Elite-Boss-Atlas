0ae9e7130089bb0b2f6fb563bba0ee3b7ada1281
souschef2403 <souschef2403@outlook.com>
2026-07-28 03:53:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.ATLAS_CONFIG = {
   supabaseUrl: "https://tfsmdjhhudooraaoywpa.supabase.co",
   supabaseAnonKey: "sb_publishable_Rw8Utdd1H8Xr7chA8Wrk5A_MKOEH2t3",
   atlasName: "Elite Boss Locations",
-  guildName: "Dark Destoyers",
+  guildName: "Thrones and Liberty",
   publicRead: true
 };

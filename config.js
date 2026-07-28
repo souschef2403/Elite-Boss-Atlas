154c927b4591f1093b29d840bdcf31fb99411e7b
souschef2403 <souschef2403@outlook.com>
2026-07-28 02:09:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ATLAS_CONFIG = {
   supabaseUrl: "https://tfsmdjhhudooraaoywpa.supabase.co",
   supabaseAnonKey: "sb_publishable_Rw8Utdd1H8Xr7chA8Wrk5A_MKOEH2t3",
-  atlasName: "Elite Boss Atlas",
+  atlasName: "Elite Boss Locations",
   guildName: "Dark Destoyers",
   publicRead: true
 };
